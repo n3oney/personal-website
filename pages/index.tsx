211871e52f881562bd5ef16d3f7d53c9a5060d3e
n3oney <neoney@pm.me>
2021-06-17 22:06:36 +0200

    feat: create homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,27 @@
+import React from "react";
+import Link from "next/link";
+import Animation from "../components/Animation";
+
 export default function Home() {
   return (
-    <div>
-      Hello World!
+    <div className="flex flex-col lg:flex-row h-full justify-center mx-4 items-center">
+      <div className="flex flex-col justify-center text-center items-center mt-16 mb-4 lg:my-0">
+        <h1 className="font-bold text-6xl text-pink-1 mb-4">neoney</h1>
+        <h3 className="font-bold text-xl text-pink-2">A 16 year-old web developer<br/>from Poland.</h3>
+        <div className="grid grid-cols-2 mt-8 gap-x-2.5 place-items-stretch">
+          <Link href="/projects">
+            <a className="bg-pink-1 text-gray-6 rounded-md px-3 py-2 text-sm shadow-light flex items-center justify-center">
+              Check my projects out <span className="ml-2 whitespace-nowrap">-&gt;</span>
+            </a>
+          </Link>
+          <Link href="/nexe">
+          <a className="bg-pink-1 text-gray-6 rounded-md px-3 py-2 text-sm shadow-light flex items-center justify-center">
+              Nexe <span className="ml-2 whitespace-nowrap">-&gt;</span>
+            </a>
+          </Link>
+        </div>
+      </div>
+      <Animation/>
     </div>
   );
 }
