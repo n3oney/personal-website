873a91f88e19b0b663671a201fac4f577394169a
n3oney <neoney@pm.me>
2021-06-17 20:49:04 +0200

    feat: add page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import React from 'react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>neoney.me</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp;
