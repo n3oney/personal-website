0d99f382cc297f493fa60fb4b56365572d7ec1eb
neoney <neoney@pm.me>
2021-06-18 16:24:33 +0200

    feat: create Navbar.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import React from 'react';
 import Head from 'next/head';
+import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
     <Head>
       <title>neoney.me</title>
     </Head>
+    <Navbar />
     <Component {...pageProps} />
   </>
 }
