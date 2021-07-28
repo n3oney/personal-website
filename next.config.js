6ae0f3abc014fb0f6eb7004a6b8ccb52f8d26f86
neoney <neoney@pm.me>
2021-07-29 00:25:48 +0200

    feat: add redirect to new nexe page

--- next.config.js
+++ next.config.js
@@ -4,5 +4,14 @@ const withTM = require("next-transpile-modules")([
 ])
 
 module.exports = withTM({
-  reactStrictMode: true
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/nexe',
+        destination: `https://nexe.${process.env.NEXT_PUBLIC_BASE_DOMAIN}/`,
+        permanent: true
+      }
+    ]
+  }
 });
