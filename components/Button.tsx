649aef3a25a0671452d718752430ecad2e7b8fb1
neoney <neoney@pm.me>
2021-11-01 13:56:23 +0100

    refactor: change button styling

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,8 +1,18 @@
 import React, { ButtonHTMLAttributes } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-const Button: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({ children, ...props }) => (
-  <button {...props} className={`text-pink-1 transition border-2 border-pink-1 bg-black hover:bg-pink-1 hover:text-black rounded-md px-3 py-2 text-sm shadow-light flex items-center justify-center ${props.className}`}>
-      {children}
+const Button: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
+  children,
+  ...props
+}) => (
+  <button
+    {...props}
+    className={twMerge(
+      'font-bold text-pink-1 transition border-2 border-pink-1 bg-black hover:bg-pink-1 hover:text-black rounded-md px-3 py-2 text-sm flex items-center justify-center',
+      props.className
+    )}
+  >
+    {children}
   </button>
 );
 
